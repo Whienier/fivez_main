c234d12e09462b215d6b74cc6ca50a58ff056f5e
Techwarfare <daniel_rangeley@live.com>
2022-11-24 08:52:52 +1000

    Blip fix

--- client/main.lua
+++ client/main.lua
@@ -698,7 +698,7 @@ Citizen.CreateThread(function()
     for k,v in pairs(Config.Blips) do
         AddTextEntry(v.labelid, v.label)
         local blip = AddBlipForCoord(v.position.x, v.position.y, v.position.z)
-        BeginTextCommandSetBlipName(v.labelid)
+        BeginTextCommandSetBlipName(v.label)
         EndTextCommandSetBlipName(blip)
         SetBlipSprite(blip, v.sprite)
     end

1a2a08a62785f0f474667d3b027360d5bc00f189
Techwarfare <daniel_rangeley@live.com>
2022-12-02 14:52:42 +1000

    Fixes

--- client/routingportals.lua
+++ client/routingportals.lua
@@ -1,5 +1,4 @@
 local insideRoutingInterior = false
-local insideEntryMarker = false
 
 local usedInteriorId = nil
 local usedPortalId = nil
@@ -10,14 +9,16 @@ Citizen.CreateThread(function()
         while not startThreads do Citizen.Wait(1) end
         while not NetworkIsPlayerActive(PlayerId()) do Citizen.Wait(1) end
 
-        --If we are inside a routing interior
+        --If we are inside a routing interior then wait till we get out
         while insideRoutingInterior do Citizen.Wait(1) end
 
         local pedCoords = GetEntityCoords(GetPlayerPed(-1))
         local dist = -1
+        local closestCoords = nil
         local tempRoutingId = nil
         local tempInteriorId = nil
         local tempPortalId = nil
+        --Loop through routing interiors and check which is the closest
         for k,v in pairs(Config.RoutingInteriors) do
             tempRoutingId = k
             for k,house in pairs(v.inPositions) do
@@ -26,22 +27,27 @@ Citizen.CreateThread(function()
                     local distance = #(pedCoords - portalPos)
 
                     if dist == -1 or distance < dist then
+                        closestCoords = portalPos
                         dist = distance
                         tempPortalId = k
                     end
                 end
             end
         end
-
-        if dist <= 15 then
-            DrawMarker(1, v.traders.barber.x, v.traders.barber.y, v.traders.barber.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0, 255, 0, 255, true, false, 2, false, nil, nil, false)
-        end
-
-        if dist <= 3 then
-            if IsControlJustPressed(0, 191) then
-                usedInteriorId = tempInteriorId
-                usedPortalId = tempPortalId
-                TriggerServerEvent("fivez:EnterRoutingPortal", tempRoutingId, tempInteriorId, tempPortalId)
+        --Make sure we have a distance and the distance is less than 16
+        if dist ~= -1 and dist < 16 then
+            --Draw marker if we are close enough
+            if dist <= 15 then
+                DrawMarker(1, closestCoords.x, closestCoords.y, closestCoords.z, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0, 255, 0, 255, true, false, 2, false, nil, nil, false)
+            end
+            --Check for inputs if we are close enough
+            if dist <= 3 then
+                if IsControlJustPressed(0, 191) then
+                    usedInteriorId = tempInteriorId
+                    usedPortalId = tempPortalId
+                    TriggerServerEvent("fivez:EnterRoutingPortal", tempRoutingId, tempInteriorId, tempPortalId)
+                    insideRoutingInterior = true
+                end
             end
         end
         Citizen.Wait(1)
@@ -53,7 +59,7 @@ Citizen.CreateThread(function()
     while true do
         while not startThreads do Citizen.Wait(1) end
         while not NetworkIsPlayerActive(PlayerId()) do Citizen.Wait(1) end
-        --If we are not inside a routing interior
+        --If we are not inside a routing interior wait until we are inside one
         while not insideRoutingInterior do Citizen.Wait(1) end
 
         local pedCoords = GetEntityCoords(GetPlayerPed(-1))
@@ -80,6 +86,7 @@ Citizen.CreateThread(function()
                     TriggerServerEvent("fivez:ExitRoutingPortal", tempRoutingId, usedInteriorId, usedPortalId)
                     usedInteriorId = nil
                     usedPortalid = nil
+                    insideRoutingInterior = false
                 end
             end
         end
--- server/routingmanager.lua
+++ server/routingmanager.lua
@@ -58,13 +58,19 @@ function AddActiveInterior(routingId, interiorId, source)
             source
         }
     })
+    --Disable client-side entity creation
+    SetRoutingBucketEntityLockdownMode(routingBucket, "strict")
+    SetRoutingBucketPopulationEnabled(routingBucket, false)
+    --Set the player going into the interior to the routing bucket
     SetPlayerRoutingBucket(source, routingBucket)
 end
 
 function AddPlayerToActiveInterior(interiorId, source)
     for k,v in pairs(activeInteriors) do
         if v.interiorId == interiorId then
+            --Set player to the active interiors routing bucket
             SetPlayerRoutingBucket(source, v.routingBucket)
+            --Also insert them into the players table
             table.insert(v.players, source)
         end
     end
@@ -76,7 +82,9 @@ function RemovePlayerFromActiveInterior(interiorId, source)
             local activeInteriorId = k
             for k,v in pairs(v.players) do
                 if v == source then
+                    --Set player to default routing bucket
                     SetPlayerRoutingBucket(source, 0)
+                    --Remove player from the players table
                     table.remove(activeInteriors[activeInteriorId].players, k)
                 end
             end
@@ -88,14 +96,20 @@ RegisterNetEvent("fivez:EnterRoutingPortal", function(routingId, interiorId, por
     local source = source
     local playerPed = GetPlayerPed(source)
     local routingPortal = Config.RoutingInteriors[routingId].inPositions[interiorId][portalId]
+    --Make sure player is close enough to portal to use it
     if #(GetEntityCoords(playerPed) - routingPortal) <= 3 then
+        --Get any active interiors with the same interiorId
         local activeInterior = GetActiveInterior(interiorId)
         if activeInterior ~= nil then
+            --If we find an active interior that is available add player to it
             AddPlayerToActiveInterior(interiorId, source)
         else
+            --If we didn't find an active interior, create one for the player
             AddActiveInterior(routingId, interiorId, source)
         end
+        --Get the location of the exit portal
         local exitPortal = Config.RoutingInteriors[routingId].outPosition
+        --Set player ped coords to the exit portal
         SetEntityCoords(GetPlayerPed(source), exitPortal.x, exitPortal.y, exitPortal.z, true, false, false, false)
     else
         print(routingPortal," - Not close enough to routing portal!", routingId, interiorId, portalId)

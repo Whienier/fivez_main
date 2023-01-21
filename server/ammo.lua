55470a1bf703e4ae03b3f290c197581f8b233d9c
Techwarfare <daniel_rangeley@live.com>
2023-01-22 02:28:34 +1000

    Update
    -Decreases attachments attached to guns quality when shot using it

--- server/ammo.lua
+++ server/ammo.lua
@@ -69,12 +69,23 @@ RegisterNetEvent("fivez:ShootingWeapon", function(weaponHash)
         SQL_SetWeaponAmmoCount(playerData.Id, weaponHash, ammoCountCB)
 
         local usingSlot = playerData.characterData.inventory.hands
+        local hasAttachment = false
         if GetHashKey(playerData.characterData.inventory.items[usingSlot].model) == weaponHash then
+            for k,v in pairs(playerData.characterData.inventory.items[usingSlot].attachments) do
+                local configItem = Config.Items[Config.GetItemWithModel(k).itemId]
+                if not configItem.isMag then
+                    playerData.characterData.inventory.items[usingSlot].attachments[k] = v - 1
+                    hasAttachment = true
+                end
+            end
             playerData.characterData.inventory.items[usingSlot].quality = playerData.characterData.inventory.items[usingSlot].quality - 1
             if playerData.characterData.inventory.items[usingSlot].quality <= 0 then
                 playerData.characterData.inventory.items[usingSlot].quality = 0
                 playerData.characterData.inventory.hands = -1
                 SetCurrentPedWeapon(GetPlayerPed(source), GetHashKey("weapon_unarmed"), true)
+            end
+            if hasAttachment then
+                SQL_UpdateItemAttachmentsInCharacterInventory(playerData.Id, usingSlot, playerData.characterData.inventory.items[usingSlot].attachments)
             end
             SQL_UpdateItemQualityInCharacterInventory(playerData.Id, usingSlot, playerData.characterData.inventory.items[usingSlot].quality)
             TriggerClientEvent("fivez:UpdateInventoryItemQuality", source, usingSlot, playerData.characterData.inventory.items[usingSlot].quality)
